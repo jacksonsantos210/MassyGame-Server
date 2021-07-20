d70bd87b4cf931d2e5e986cc3ef4c4a07b631208
jackson-dos-santos <jackson144@gmail.com>
2021-07-20 15:53:14 -0300

    Update 20210717015134-create-albuns.js

--- src/database/migrations/20210717015134-create-albuns.js
+++ src/database/migrations/20210717015134-create-albuns.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("players", {
+    await queryInterface.createTable("albuns", {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
