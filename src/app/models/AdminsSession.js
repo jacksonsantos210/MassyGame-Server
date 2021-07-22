401e94eb61e276738389c62df7958e858dd9611f
jackson-dos-santos <jackson144@gmail.com>
2021-07-22 16:51:05 -0300

    Create AdminsSession.js

---
+++ src/app/models/AdminsSession.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require("sequelize");
+
+class AdminsSession extends Model {
+  static init(sequelize) {
+    console.log("APP-> Model: AdminsSession, has been initialized");
+    super.init(
+      {
+        admin_id: DataTypes.INTEGER,
+        token: DataTypes.STRING,
+        ip_address: DataTypes.STRING,
+        user_agent: DataTypes.STRING,
+        payload: DataTypes.STRING,
+        logged: DataTypes.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+module.exports = AdminsSession;
