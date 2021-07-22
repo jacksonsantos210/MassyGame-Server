d55d6b0c6826671740b9b155647f62a9ac426f17
jackson-dos-santos <jackson144@gmail.com>
2021-07-22 16:51:50 -0300

    Update 20210717015142-create-stands.js

--- src/database/migrations/20210717015142-create-stands.js
+++ src/database/migrations/20210717015142-create-stands.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "figures",
+          model: "albums",
           key: "id",
         },
       },
