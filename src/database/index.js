d62fab5fe1d5f3b19e17d8f74d9af0de89e52cb0
jackson-dos-santos <jackson144@gmail.com>
2021-07-22 16:51:38 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,30 @@
 const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
 // import models
-const Player = require("../app/models/Player");
-const Figure = require("../app/models/Figure");
-const Rank = require("../app/models/Rank");
+const Admin = require("../app/models/Admin");
+const AdminsSession = require("../app/models/AdminsSession");
 const Album = require("../app/models/Album");
+const Developer = require("../app/models/Developer");
+const DevelopersSession = require("../app/models/DevelopersSession");
+const Figure = require("../app/models/Figure");
+const Log = require("../app/models/Log");
+const Player = require("../app/models/Player");
+const PlayersSession = require("../app/models/PlayersSession");
+const Premier = require("../app/models/Premier");
+const Rank = require("../app/models/Rank");
+
 //start connection
 const connection = new Sequelize(dbConfig);
-Player.init(connection);
-Figure.init(connection);
-Rank.init(connection);
+Admin.init(connection);
+AdminsSession.init(connection);
 Album.init(connection);
+Developer.init(connection);
+DevelopersSession.init(connection);
+Figure.init(connection);
+Log.init(connection);
+Player.init(connection);
+PlayersSession.init(connection);
+Premier.init(connection);
+Rank.init(connection);
 
 module.exports = connection;
