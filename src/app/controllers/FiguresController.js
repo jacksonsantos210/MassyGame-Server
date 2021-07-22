51a2da2041f95faba5c743e0f2be0dc293c6cf39
jackson-dos-santos <jackson144@gmail.com>
2021-07-22 16:50:33 -0300

    Update FiguresController.js

--- src/app/controllers/FiguresController.js
+++ src/app/controllers/FiguresController.js
@@ -1,4 +1,5 @@
 const Figure = require("../models/Figure");
+const Premier = require("../models/Premier");
 const FigureSchema = require("../yup/FigureSchema");
 
 class FiguresController {
@@ -9,23 +10,23 @@ class FiguresController {
         figures: figures,
       });
     } catch (error) {
-      return res.status(400).json({
+      return res.status(500).json({
         message: "Erro ao tentar listar figurinhas",
       });
     }
   }
-  /* 
+
   async show(req, res) {
     try {
-      const figure = await Figure.findById(req.params.id);
-      res.status(200).json(figure);
+      const figure = await Figure.findOne({ where: { id: req.params.id } });
+      res.status(200).json({ figure: figure });
     } catch (error) {
       console.log(error);
       res.status(500).json({
         message: "Falha ao carregar a figurinha",
       });
     }
-  } */
+  }
 
   async store(req, res) {
     try {
@@ -37,13 +38,40 @@ class FiguresController {
       const figure = await Figure.create(req.body);
       return res.status(200).json({
         message: "Figurinha Cadastrado com sucesso",
-
         figure: figure,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        message: "Erro ao tentar inserir figurinha",
+      });
+    }
+  }
+
+  async getPremier(req, res) {
+    try {
+      const valid = await Premier.update(
+        { opened: true },
+        {
+          where: {
+            [Op.and]: [
+              { player_id: req.params.id },
+              { hash: req.body.hash },
+              { opened: false },
+            ],
+          },
+        }
+      );
+      console.log(valid);
+      return res.status(200).json({
+        message: "Figurinha Cadastrado com sucesso",
+
+        figure: valid,
       });
     } catch (e) {
       console.error(e);
-      return res.status(400).json({
-        message: "Erro ao tentar inserir figurinha",
+      return res.status(500).json({
+        message: "Erro ao libear premio",
       });
     }
   }
