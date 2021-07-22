bf1c8af44613311dc632822b698291387127e0cb
jackson-dos-santos <jackson144@gmail.com>
2021-07-22 16:51:43 -0300

    Create 20210717015134-create-albums.js

---
+++ src/database/migrations/20210717015134-create-albums.js
@@ -0,0 +1,61 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("albums", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      player_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "players",
+          key: "id",
+        },
+      },
+      figure_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "figures",
+          key: "id",
+        },
+      },
+      origin: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      pasted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      sold: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      sale: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("albums");
+  },
+};
