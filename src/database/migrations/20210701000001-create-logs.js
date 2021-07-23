c6afa459bcb0af64bba3e9cf35a23b35ebe36bc8
jackson-dos-santos <jackson144@gmail.com>
2021-07-23 20:38:32 -0300

    Create 20210701000001-create-logs.js

---
+++ src/database/migrations/20210701000001-create-logs.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("logs", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      action: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      information: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      level: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        dafaultValue: "player",
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("logs");
+  },
+};
