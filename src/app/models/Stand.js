a0956663e9c648e860d62a377afd3c46616e77f7
jackson-dos-santos <jackson144@gmail.com>
2021-07-26 20:02:49 -0300

    Update Stand.js

--- src/app/models/Stand.js
+++ src/app/models/Stand.js
@@ -2,11 +2,11 @@ const { Model, DataTypes } = require("sequelize");
 
 class Stand extends Model {
   static init(sequelize) {
-    console.log("APP-> Model: Figure, has been initialized");
+    console.log("APP-> Model: Stand, has been initialized");
     super.init(
       {
         player_id: DataTypes.INTEGER,
-        figure_id: DataTypes.INTEGER,
+        album_id: DataTypes.INTEGER,
         price: DataTypes.INTEGER,
         sold: DataTypes.BOOLEAN,
         sold_when: DataTypes.INTEGER,
@@ -19,7 +19,11 @@ class Stand extends Model {
   }
 
   static associate(models) {
-    this.hasMany(models.Album, { foreingKey: "figure_id", as: "albums" });
+    this.belongsTo(models.Album, {
+      foreingKey: "album_id",
+      as: "album",
+    });
+    // this.hasMany(models.Album, { foreingKey: "album_id", as: "stands_album" });
   }
 }
 
