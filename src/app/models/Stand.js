36e5bd89bd529e79ec6f9f234275b77d047bc168
jackson-dos-santos <jackson144@gmail.com>
2021-07-27 16:39:03 -0300

    Update Stand.js

--- src/app/models/Stand.js
+++ src/app/models/Stand.js
@@ -5,9 +5,8 @@ class Stand extends Model {
     console.log("APP-> Model: Stand, has been initialized");
     super.init(
       {
-        player_id: DataTypes.INTEGER,
-        album_id: DataTypes.INTEGER,
-        price: DataTypes.INTEGER,
+        seller: DataTypes.INTEGER,
+        figure_id: DataTypes.INTEGER,
         sold: DataTypes.BOOLEAN,
         sold_when: DataTypes.INTEGER,
         sold_at: DataTypes.DATE,
@@ -19,11 +18,7 @@ class Stand extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.Album, {
-      foreingKey: "album_id",
-      as: "album",
-    });
-    // this.hasMany(models.Album, { foreingKey: "album_id", as: "stands_album" });
+    this.belongsTo(models.Figure, { foreingKey: "figure_id", as: "figure" });
   }
 }
 
