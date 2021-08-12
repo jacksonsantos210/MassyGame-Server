faf180afcaa8e3becac857f811056e7c6e09ca44
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 03:02:12 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,8 +5,10 @@ module.exports = {
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "DEV1@jssolucoes",
   database: process.env.DB_DATABASE || "masseygame_dev",
+  logging: false,
   define: {
     timestamps: true,
     underscored: true,
+    logging: false,
   },
 };
