2c8247fddb42478f610a415b263144b1886be2b8
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:50:29 -0300

    Update Stand.js

--- src/app/models/Stand.js
+++ src/app/models/Stand.js
@@ -2,7 +2,6 @@ const { Model, DataTypes } = require("sequelize");
 
 class Stand extends Model {
   static init(sequelize) {
-    console.log("APP-> Model: Stand, has been initialized");
     super.init(
       {
         seller: DataTypes.INTEGER,
