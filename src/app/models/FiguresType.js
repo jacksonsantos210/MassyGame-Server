02a4fd91471347ab169a53544687f8627a0125cf
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:51:00 -0300

    Update FiguresType.js

--- src/app/models/FiguresType.js
+++ src/app/models/FiguresType.js
@@ -2,7 +2,6 @@ const { Model, DataTypes } = require("sequelize");
 
 class FiguresType extends Model {
   static init(sequelize) {
-    console.log("APP-> Model: FiguresType, has been initialized");
     super.init(
       {
         name: DataTypes.STRING,
