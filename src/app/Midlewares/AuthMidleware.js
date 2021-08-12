4c644565cce95b517d4cecaf029840740c6668f1
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:49:59 -0300

    Update AuthMidleware.js

--- src/app/Midlewares/AuthMidleware.js
+++ src/app/Midlewares/AuthMidleware.js
@@ -1,40 +1,38 @@
 const jwt = require("jsonwebtoken");
 const config = require("../../config/auth");
-const { promisify } = require('util');
+const { promisify } = require("util");
 
 module.exports = async (req, res, next) => {
   const auth = req.headers.authorization;
 
-  if(!auth){
+  if (!auth) {
     return res.status(401).json({
       error: true,
       code: 130,
-      message: "O token de autenticação não existe!"
-    })
+      message: "O token de autenticação não existe!",
+    });
   }
 
-  const [, token] = auth.split(' ');
+  const [, token] = auth.split(" ");
 
   try {
     const decoded = await promisify(jwt.verify)(token, config.secret);
-
-    if(!decoded){
+    if (!decoded) {
       return res.status(401).json({
         error: true,
         code: 130,
-        message: "O token está expirado!"
-      })
+        message: "O token está expirado!",
+      });
     } else {
       req.user_id = decoded.id;
+      req.user_type = decoded.user_type;
       next();
     }
-    
   } catch {
     return res.status(401).json({
       error: true,
       code: 130,
-      message: "O token está inválido!"
-    })
+      message: "O token está inválido!",
+    });
   }
-
-}
+};
