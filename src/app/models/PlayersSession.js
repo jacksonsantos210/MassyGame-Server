1a3d0f90b31ff59f54734598e0d8437c0ab87569
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:50:38 -0300

    Update PlayersSession.js

--- src/app/models/PlayersSession.js
+++ src/app/models/PlayersSession.js
@@ -2,7 +2,6 @@ const { Model, DataTypes } = require("sequelize");
 
 class PlayersSession extends Model {
   static init(sequelize) {
-    console.log("APP-> Model: PlayersSession, has been initialized");
     super.init(
       {
         player_id: DataTypes.INTEGER,
