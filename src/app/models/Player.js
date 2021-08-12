bc3b827e2cc81d064b5d3bfface84f2c2f8ef4f5
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:50:53 -0300

    Update Player.js

--- src/app/models/Player.js
+++ src/app/models/Player.js
@@ -2,7 +2,6 @@ const { Model, DataTypes } = require("sequelize");
 
 class Player extends Model {
   static init(sequelize) {
-    console.log("APP-> Model: Player, has been initialized");
     super.init(
       {
         name: DataTypes.STRING,
