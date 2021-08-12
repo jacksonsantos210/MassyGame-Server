fb3609d46acc43196871812fe906a87801bf0a1b
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:51:05 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-console.clear();
-console.log("Start APP Server");
 require("dotenv/config");
+const http = require("http");
 const app = require("./app");
 
 var PORT = process.env.PORT || 3333;
-app.listen(PORT, () => {
+
+http.createServer(app).listen(PORT, () => {
   console.log(`APP listenner on port ${PORT}`);
 });
