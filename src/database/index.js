fc7b20c2adabb628c3a9bad7e9621f9596945d7c
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:50:24 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -7,7 +7,6 @@ const Album = require("../app/models/Album");
 const Developer = require("../app/models/Developer");
 const DevelopersSession = require("../app/models/DevelopersSession");
 const Figure = require("../app/models/Figure");
-const Log = require("../app/models/Log");
 const Player = require("../app/models/Player");
 const PlayersSession = require("../app/models/PlayersSession");
 const Premier = require("../app/models/Premier");
@@ -23,18 +22,15 @@ Developer.init(connection);
 DevelopersSession.init(connection);
 FiguresType.init(connection);
 Figure.init(connection);
-Log.init(connection);
 Player.init(connection);
 PlayersSession.init(connection);
 Premier.init(connection);
 Stand.init(connection);
 //Associates
-console.log("APP-> Model-Associations: registrations in initializing");
 Album.associate(connection.models);
 Figure.associate(connection.models);
 Player.associate(connection.models);
 Stand.associate(connection.models);
 Premier.associate(connection.models);
-console.log("APP-> Model-Associations: registrations has been initialized");
 
 module.exports = connection;
