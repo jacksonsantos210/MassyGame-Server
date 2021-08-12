387779ea7b261077b74797cfffbce702e863cc64
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:50:14 -0300

    Update Developer.js

--- src/app/models/Developer.js
+++ src/app/models/Developer.js
@@ -2,7 +2,6 @@ const { Model, DataTypes } = require("sequelize");
 
 class Developer extends Model {
   static init(sequelize) {
-    console.log("APP-> Model: Developer, has been initialized");
     super.init(
       {
         name: DataTypes.STRING,
