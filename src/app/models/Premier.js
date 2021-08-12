bd19019d46727cf727477e1ad936b4540090736b
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:50:31 -0300

    Update Premier.js

--- src/app/models/Premier.js
+++ src/app/models/Premier.js
@@ -2,7 +2,6 @@ const { Model, DataTypes } = require("sequelize");
 
 class Premier extends Model {
   static init(sequelize) {
-    console.log("APP-> Model: Premier, has been initialized");
     super.init(
       {
         date: DataTypes.DATE,
