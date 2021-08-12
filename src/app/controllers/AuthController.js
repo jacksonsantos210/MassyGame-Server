e1a34d1ac9505bf9d4f82dfd7fdeae07cea2f38a
jackson-dos-santos <jackson144@gmail.com>
2021-08-12 02:49:53 -0300

    Update AuthController.js

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,4 +1,3 @@
-const Logs = require("./LogsController");
 const Player = require("../models/Player");
 const PlayersSession = require("../models/PlayersSession");
 const Developer = require("../models/Developer");
@@ -11,79 +10,48 @@ const config = require("../../config/auth");
 const AuthSchema = require("../yup/AuthSchema");
 
 class AuthController {
-  async me(req, res) {
-    var user = null;
-    if (req.params.type === "player")
-      user = await Player.findOne({
-        where: { id: req.user_id },
-        attributes: { exclude: ["password"] },
-        include: { association: "albums" },
-      });
-    if (req.params.type === "admin")
-      user = await Admin.findOne({
-        where: { id: req.user_id },
-        attributes: { exclude: ["password"] },
-      });
-    if (req.params.type === "dev")
-      user = await Developer.findOne({
-        where: { id: req.user_id },
-        attributes: { exclude: ["password"] },
-      });
-    return res.status(200).json(user);
-  }
-
   async playerLogIn(req, res) {
     try {
       const { email, password } = req.body;
       if (!(await AuthSchema.isValid(req.body))) {
-        await Logs.save("validate-invalid", `login atempt: ${email}`, "player");
         return res.status(400).json({
           message: "Ops! Dados Inválidos.",
         });
       }
-
-      await Logs.save("login_attempt", `login atempt: ${email}`, "player");
       const player = await Player.findOne({
         where: { email: email },
         include: { association: "albums" },
       });
       if (!player) {
-        await Logs.save(
-          "login_error",
-          `player: ${req.body.email} not found`,
-          "player"
-        );
         return res.status(400).json({
           message: "Ops! Dados incorretos ou jogador inexistente.  001",
         });
       }
       if (!(await bcrypt.compare(password, player.password))) {
-        await Logs.save(
-          "login_error",
-          `player: ${req.body.email} password error`,
-          "player"
-        );
         return res.status(400).json({
           message: "Ops! Dados incorretos ou jogador inexistente.  002",
         });
       }
-      await Logs.save("login_success", `player: ${req.body.email}`, "player");
-      const token = jwt.sign({ id: player.id }, config.secret, {
-        expiresIn: config.expireIn,
-      });
-      await Logs.save(
-        "register_session",
-        `create session to player : ${player.id}`,
-        "player"
+      const token = jwt.sign(
+        { id: player.id, user_type: "player" },
+        config.secret,
+        {
+          expiresIn: config.expireIn,
+        }
       );
+      const ipCliente =
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
       await PlayersSession.create({
         player_id: player.id,
         token: token,
-        ip_address: "",
-        user_agent: "",
+        ip_address: ipCliente,
+        user_agent: req.get("user-agent"),
         payload: "",
         logged: true,
       });
+
       player.password = undefined;
       return res.status(200).json({
         token: token,
@@ -91,7 +59,6 @@ class AuthController {
       });
     } catch (error) {
       console.error(error);
-      await Logs.save("login_fail", `player: ${req.body.email}`, "player");
       return res.status(400).json({
         message: "Ops! Falha ao validar seus dados",
       });
@@ -100,24 +67,21 @@ class AuthController {
 
   async logout(req, res) {
     try {
-      const { type } = req.params;
-      console.log(req);
-      const { token } = req.token;
       let valid = null;
-      if (type === "player") {
+      if (req.user_type === "player") {
         valid = await PlayersSession.update(
           { logged: false },
-          { where: { token: token } }
+          { where: { player_id: req.user_id } }
         );
-      } else if (type === "developer") {
+      } else if (req.user_type === "developer") {
         valid = await DevelopersSession.update(
           { logged: false },
-          { where: { token: token } }
+          { where: { admin_id: req.user_id } }
         );
-      } else if (type === "admin") {
+      } else if (req.user_type === "admin") {
         valid = await AdminsSession.update(
           { logged: false },
-          { where: { token: token } }
+          { where: { developer_id: req.user_id } }
         );
       }
       return res.status(200).json({
