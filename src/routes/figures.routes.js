6773f8fc3764edfec4c6870b98c1bcecee0c5077
jackson-dos-santos <jackson144@gmail.com>
2021-08-13 09:09:09 -0300

    Update figures.routes.js

--- src/routes/figures.routes.js
+++ src/routes/figures.routes.js
@@ -4,7 +4,8 @@ const Controller = require("../app/controllers/FiguresController");
 const FiguresRoutes = new Router();
 const routes = FiguresRoutes;
 routes.get("/figures", Midleware, Controller.index);
+routes.get("/figures/:id", Midleware, Controller.show);
 routes.get("/figures/premier/verify", Midleware, Controller.verifyPremier);
-routes.get("/figures/premier/open", Midleware, Controller.getPremier);
+routes.post("/figures/premier/open", Midleware, Controller.getPremier);
 /* routes.post("/figures/create", Midleware, Controller.store); */
 module.exports = FiguresRoutes;
