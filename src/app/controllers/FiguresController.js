81c49bca64ac26734f44860593f7d251b393ee1e
jackson-dos-santos <jackson144@gmail.com>
2021-08-13 09:08:43 -0300

    Update FiguresController.js

--- src/app/controllers/FiguresController.js
+++ src/app/controllers/FiguresController.js
@@ -1,6 +1,10 @@
+const bcrypt = require("bcryptjs");
+const moment = require("moment");
 const Logger = require("../utils/logger");
 const Figure = require("../models/Figure");
 const Premier = require("../models/Premier");
+const Player = require("../models/Player");
+const Album = require("../models/Album");
 const FigureSchema = require("../yup/FigureSchema");
 
 class FiguresController {
@@ -11,6 +15,7 @@ class FiguresController {
         figures: figures,
       });
     } catch (error) {
+      Logger.game("error", "FiguresController.index -> ERROR: " + error);
       return res.status(500).json({
         message: "Erro ao tentar listar figurinhas",
       });
@@ -22,7 +27,7 @@ class FiguresController {
       const figure = await Figure.findOne({ where: { id: req.params.id } });
       res.status(200).json({ figure: figure });
     } catch (error) {
-      console.log(error);
+      Logger.game("error", "FiguresController.show -> ERROR: " + error);
       res.status(500).json({
         message: "Falha ao carregar a figurinha",
       });
@@ -42,7 +47,7 @@ class FiguresController {
         figure: figure,
       });
     } catch (error) {
-      console.error(error);
+      Logger.game("error", "FiguresController.store -> ERROR: " + error);
       return res.status(500).json({
         message: "Erro ao tentar inserir figurinha",
       });
@@ -51,26 +56,35 @@ class FiguresController {
 
   async getPremier(req, res) {
     try {
-      const valid = await Premier.update(
-        { opened: true },
-        {
-          where: {
-            [Op.and]: [
-              { player_id: req.params.id },
-              { hash: req.body.hash },
-              { opened: false },
-            ],
-          },
-        }
-      );
-      console.log(valid);
-      return res.status(200).json({
-        message: "Figurinha Cadastrado com sucesso",
-
-        figure: valid,
+      const premier = await Premier.findOne({
+        where: {
+          hash: req.body.hash,
+          opened: false,
+        },
       });
-    } catch (e) {
-      console.error(e);
+      if (!premier) {
+        return res.status(400).json({
+          message: "Este premio já foi resgatado!",
+        });
+      }
+      await Premier.update({ opened: true }, { where: { id: premier.id } });
+      const player = await Player.findByPk(req.user_id);
+      const { coin } = await Figure.findOne({
+        where: { id: premier.figure_id },
+      });
+      let scoreNew = player.score + coin;
+      await Player.update({ score: scoreNew }, { where: { id: req.user_id } });
+      await Album.create({
+        player_id: req.user_id,
+        figure_id: premier.figure_id,
+        origin: "premier",
+      });
+      return res.status(200).json({
+        message: "Figurinha aberta com sucesso",
+        premier: premier.figure_id,
+      });
+    } catch (error) {
+      Logger.game("error", "FiguresController.getPremier -> ERROR: " + error);
       return res.status(500).json({
         message: "Erro ao libear premio",
       });
@@ -79,9 +93,46 @@ class FiguresController {
 
   async verifyPremier(req, res) {
     try {
-      return res.status(200).json({ status: "ok" });
+      let premier = null;
+      premier = await Premier.findOne({
+        where: {
+          player_id: req.user_id,
+          date: moment().format("YYYY-MM-DD"),
+        },
+      });
+      if (premier === null) {
+        const hash = await bcrypt.hash(
+          `user-${req.user_id}-${moment().format("YYYYMMDDHHmmSS")}`,
+          10
+        );
+        let sort = Math.floor(Math.random() * (60 - 0) + 0);
+        await Premier.create({
+          date: moment().format("YYYY-MM-DD"),
+          player_id: req.user_id,
+          hash: hash,
+          figure_id: sort,
+          opened: false,
+        });
+        return res.status(200).json({ result: "new", hash: hash });
+      } else {
+        if (premier.opened === false) {
+          return res
+            .status(200)
+            .json({ premier: "closed", hash: premier.hash });
+        } else {
+          return res
+            .status(200)
+            .json({ premier: "opened", figure: premier.figure_id });
+        }
+      }
     } catch (error) {
-      console.error(error);
+      Logger.game(
+        "error",
+        "FiguresController.verifyPremier -> ERROR: " + error
+      );
+      return res.status(500).json({
+        message: "Erro ao verificar premio",
+      });
     }
   }
 }
