fc7425f6dfd1c28dd2fd7449e01415cb551e26ca
jackson-dos-santos <jackson144@gmail.com>
2021-08-16 12:21:52 -0300

    Update RankController.js

--- src/app/controllers/RankController.js
+++ src/app/controllers/RankController.js
@@ -1,22 +1,38 @@
 const Logger = require("../utils/logger");
 const Player = require("../models/Player");
 
+const LIMIT = 10;
+
 class RankController {
   async index(req, res) {
     try {
+      let { page = 1 } = req.query;
+      page = parseInt(page - 1);
       const top = await Player.findAll({
         order: [["score", "DESC"]],
         limit: 3,
         attributes: { exclude: ["password"] },
       });
-      const players = await Player.findAndCountAll({
+      const { count: size, rows: players } = await Player.findAndCountAll({
         order: [["score", "DESC"]],
-        limit: 100,
+        limit: LIMIT,
+        offset: page * LIMIT,
         attributes: { exclude: ["password"] },
       });
+      let pages = 0;
+      if (size <= 100) {
+        pages = Math.ceil(size / LIMIT);
+      } else {
+        pages = 10;
+      }
       return res.status(200).json({
         top: top,
-        rank: players,
+        rank: {
+          size,
+          pages,
+          actual: page + 1,
+          players,
+        },
       });
     } catch (error) {
       console.error(error);
