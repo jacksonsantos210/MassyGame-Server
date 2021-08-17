0180356102fca3d0d3041bf9be875bfa7a47d914
jackson-dos-santos <jackson144@gmail.com>
2021-08-17 17:00:19 -0300

    Update player.routes.js

--- src/routes/player.routes.js
+++ src/routes/player.routes.js
@@ -4,7 +4,7 @@ const Controller = require("../app/controllers/PlayersController");
 const PlayerRoutes = new Router();
 var routes = PlayerRoutes;
 routes.get("/players", Midleware, Controller.index);
-routes.get("/players/me", Midleware, Controller.me);
+routes.get("/players/me", Midleware, Controller.myData);
 routes.get("/players/show/:id", Midleware, Controller.show);
 routes.post("/players/register", Controller.store); //Função Pública de Registro
 routes.post("/players/update", Midleware, Controller.update);
