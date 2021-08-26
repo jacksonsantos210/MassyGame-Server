e0a5aed202c602f5d7e1d6919b1b19b95882cc7e
jackson-dos-santos <jackson144@gmail.com>
2021-08-26 04:44:21 -0300

    Create 20210825104121-create-influencers_tokens.js

---
+++ src/database/migrations/20210825104121-create-influencers_tokens.js
@@ -0,0 +1,61 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("influencers_tokens", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      influencer_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "influencers", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      player_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: "players", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      opened: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        dafaultValue: false,
+      },
+      figure_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "figures", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      opened_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("influencers_tokens");
+  },
+};
