5bc39a8a0afd280a04b896434f52a81caa41fcc2
jackson-dos-santos <jackson144@gmail.com>
2021-08-28 17:08:59 -0300

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,7 @@ const Controller = require("../app/controllers/AuthController");
 const AuthRoutes = new Router();
 const routes = AuthRoutes;
 routes.post("/auth/player", Controller.playerLogIn);
-//routes.post("/auth/admin", Controller.adminLogIn);
+routes.post("/auth/admin", Controller.adminLogIn);
 //routes.post("/auth/developer", Controller.developerLogIn);
 routes.get("/logout", Midleware, Controller.logout);
 module.exports = AuthRoutes;
