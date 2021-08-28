42aba47ac4e3df5d5f57b6f33431cea5804cf8cd
jackson-dos-santos <jackson144@gmail.com>
2021-08-28 17:08:55 -0300

    Create admin.routes.js

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const Midleware = require("../app/Midlewares/AuthMidleware");
+const Controller = require("../app/controllers/AdminsController");
+const AdminRoutes = new Router();
+var routes = AdminRoutes;
+routes.get("/admins", Midleware, Controller.index);
+routes.get("/admins/show/:id", Midleware, Controller.show);
+routes.post("/admins/create", Controller.store); //Função Pública de Registro
+routes.post("/admins/update", Midleware, Controller.update);
+module.exports = AdminRoutes;
