079f56a88db7fcb617ec357940b7bec02d553244
jackson-dos-santos <jackson144@gmail.com>
2021-08-30 14:37:57 -0300

    Update influencers-tokens.routes.js

--- src/routes/influencers-tokens.routes.js
+++ src/routes/influencers-tokens.routes.js
@@ -6,6 +6,7 @@ const routes = InfluencersTokensRoutes;
 const prefix = "/influencers-tokens";
 routes.get(prefix, Midleware, Controller.index);
 routes.get(`${prefix}/show/:id`, Midleware, Controller.show);
+routes.get(`${prefix}/useds`, Midleware, Controller.useds);
 routes.post(`${prefix}/insert`, Midleware, Controller.store);
 routes.post(`${prefix}/rescue`, Midleware, Controller.update);
 /* routes.get(`${prefix}/influencer/{id}`, Midleware, Controller.byInfluencer); */
