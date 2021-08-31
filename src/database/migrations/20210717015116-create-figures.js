89004074fc233dee7268e5d503eb59b324e18c67
jackson-dos-santos <jackson144@gmail.com>
2021-08-31 00:38:25 -0300

    Update 20210717015116-create-figures.js

--- src/database/migrations/20210717015116-create-figures.js
+++ src/database/migrations/20210717015116-create-figures.js
@@ -22,7 +22,7 @@ module.exports = {
         allowNull: false,
         defaultValue: 0,
       },
-      type: {
+      type_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         defaultValue: 1,
