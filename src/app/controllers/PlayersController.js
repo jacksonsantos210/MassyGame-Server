56b2743cb28f6f6eeb97cd781bcb8adbcdfc01be
jackson-dos-santos <jackson144@gmail.com>
2021-09-02 17:58:59 -0300

    Update PlayersController.js

--- src/app/controllers/PlayersController.js
+++ src/app/controllers/PlayersController.js
@@ -1,11 +1,14 @@
-const Logger = require("../utils/logger");
+require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const Logger = require("../utils/logger");
 const config = require("../../config/auth");
+const PlayerSchema = require("../yup/PlayerSchema");
 const Player = require("../models/Player");
+const PlayersToken = require("../models/PlayersToken");
 const Premier = require("../models/Premier");
 const Stand = require("../models/Stand");
-const PlayerSchema = require("../yup/PlayerSchema");
 
 class PlayersController {
   async index(req, res) {
@@ -157,7 +160,109 @@ class PlayersController {
       );
       return res.status(200).json({ message: "OK" });
     } catch (error) {
-      cLogger.game("error", "PlayersController.audio -> ERROR: " + error);
+      Logger.game("error", "PlayersController.audio -> ERROR: " + error);
+      return res.status(400).json({
+        message: "Erro ao tentar atualizar jogador",
+      });
+    }
+  }
+
+  async recovery(req, res) {
+    try {
+      const player = await Player.findOne({
+        where: { email: req.body.email },
+      });
+      let message = "";
+      if (!player) {
+        message = "Não encontramos uma conta associada a este email!";
+      } else {
+        let token = await bcrypt.hash(player.email, 10);
+        await PlayersToken.create({
+          player_id: player.id,
+          token: token,
+        });
+        message = `Olá ${
+          player.name
+        }, você solicitou a recuperação de senha? Para continuar acesse o link: 
+                   ${
+                     process.env.URL_GAME || "https://localhost:3000"
+                   }/password-recovery?token=${token}`;
+      }
+      const transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      });
+      transporter
+        .sendMail({
+          from: "Colecionáveis Massey",
+          to: "",
+          replyTo: "no-replay@masseyferguson60anos.com.br",
+          subject: "Colecionáveis Massey - Password Recovery",
+          text: message,
+        })
+        .then((info) => {
+          return res.status(200).json({
+            message: "Recuperação em processamento, verifique seu e-mail",
+          });
+        })
+        .catch((error) => {
+          Logger.game("error", "PlayersController.recovery -> ERROR: " + error);
+          return res.status(400).json({
+            message: "Erro ao processar o peido de recuperação",
+          });
+        });
+    } catch (error) {
+      Logger.game("error", "PlayersController.recovery -> ERROR: " + error);
+      return res.status(400).json({
+        message: "Erro ao tentar gerar token de recuperação",
+      });
+    }
+  }
+
+  async validateToken(req, res) {
+    try {
+      const player_token = await PlayersToken.findOne({
+        where: { token: req.body.token },
+      });
+      if (player_token) {
+        return res.status(200).json({
+          message: "ok",
+          player_id: player_token.player_id,
+        });
+      } else {
+        return res.status(500).json({
+          message: "Token Inválido",
+          player,
+        });
+      }
+    } catch (error) {
+      Logger.game("error", "PlayersController.recovery -> ERROR: " + error);
+      return res.status(400).json({
+        message: "Erro ao tentar gerar token de recuperação",
+      });
+    }
+  }
+
+  async changePassword(req, res) {
+    try {
+      await Player.update(
+        { password: bcrypt.hash(req.body.password, 10) },
+        {
+          where: { id: req.body.user_id },
+        }
+      );
+      if (req.body.origin === "recovery") {
+        await PlayersToken.destroy({
+          where: { player_id: req.body.player_id },
+        });
+      }
+      return res.status(200).json({ message: "OK" });
+    } catch (error) {
+      Logger.game("error", "PlayersController.rules -> ERROR: " + error);
       return res.status(400).json({
         message: "Erro ao tentar atualizar jogador",
       });
