ac2cdd80e44c61404990ddc06ff59cfb38d53652
jackson-dos-santos <jackson144@gmail.com>
2021-09-29 19:32:28 -0300

    Update AdminsController.js

--- src/app/controllers/AdminsController.js
+++ src/app/controllers/AdminsController.js
@@ -77,6 +77,29 @@ class AdminsController {
       });
     }
   }
+
+  async changePassword(req, res) {
+    try {
+      await Admin.update(
+        { password: bcrypt.hash(req.body.password, 10) },
+        {
+          where: { id: req.body.user_id },
+        }
+      );
+      if (req.body.origin === "recovery") {
+        await PlayersToken.destroy({
+          where: { player_id: req.body.player_id },
+        });
+      }
+      return res.status(200).json({ message: "OK" });
+    } catch (error) {
+      Logger.game("error", "AdminsController.changePassword -> ERROR: " + error);
+      return res.status(400).json({
+        message: "Erro ao tentar atualizar senha",
+      });
+    }
+  }
+}
 }
 
 module.exports = new AdminsController();
