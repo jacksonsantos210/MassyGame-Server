71bf58b724591f02f8a552a6a62480f0a21195a1
jackson-dos-santos <jackson144@gmail.com>
2021-10-05 14:24:51 -0300

    Create 20210825104125-create-influencers_tokens-useds.js

---
+++ src/database/migrations/20210825104125-create-influencers_tokens-useds.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("influencers_tokens-useds", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      token_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: "influencers_tokens", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      player_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: "players", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      opened_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("influencers_tokens");
+  },
+};
