5ea4b828bc0a1b14e6df58882ed68e37ec1fd374
jackson-dos-santos <jackson144@gmail.com>
2021-10-05 14:24:46 -0300

    Create InfluencersTokensUsed.js

---
+++ src/app/models/InfluencersTokensUsed.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+
+class InfluencersToken extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        token_id: DataTypes.INTEGER,
+        player_id: DataTypes.INTEGER,
+        opened: DataTypes.BOOLEAN,
+        opened_at: DataTypes.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+
+  /* static associate(models) {
+    this.belongsTo(models.Influencer, {
+      foreingKey: "token_id",
+      as: "tokens",
+    });
+    this.belongsTo(models.Player, { foreingKey: "player_id", as: "player" });
+  } */
+}
+
+module.exports = InfluencersToken;
