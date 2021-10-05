67ba43aec411aa03b7af452bcab7c5d0b0dbe1b4
jackson-dos-santos <jackson144@gmail.com>
2021-10-05 15:28:41 -0300

    Update 20210825104125-create-influencers_tokens-useds.js

--- src/database/migrations/20210825104125-create-influencers_tokens-useds.js
+++ src/database/migrations/20210825104125-create-influencers_tokens-useds.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("influencers_tokens-useds", {
+    await queryInterface.createTable("influencers_tokens_useds", {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
