51cbbc082d43f0f639e1796102aa7737680882a8
jackson-dos-santos <jackson144@gmail.com>
2021-10-05 15:58:45 -0300

    Update InfluencersToken.js

--- src/app/models/InfluencersToken.js
+++ src/app/models/InfluencersToken.js
@@ -15,14 +15,13 @@ class InfluencersToken extends Model {
     );
   }
 
-  /*  static associate(models) {
+  static associate(models) {
     this.belongsTo(models.Influencer, {
       foreingKey: "infulencer_id",
       as: "influencer",
     });
-    this.belongsTo(models., { foreingKey: "player_id", as: "player" });
     this.belongsTo(models.Figure, { foreingKey: "figure_id", as: "figure" });
-  } */
+  }
 }
 
 module.exports = InfluencersToken;
